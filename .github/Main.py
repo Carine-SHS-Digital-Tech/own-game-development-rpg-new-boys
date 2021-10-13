1a767d1ef2875f10a10cdbcfaeb9d48880fb1e80
TechVortexYT <geezgamer24@gmail.com>
2021-10-13 11:03:57 +0800

    0.0.0|started development and more

--- .github/Main.py
+++ .github/Main.py
@@ -18,3 +18,37 @@ COUNT = 0
 
 displaysurface = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("The Heroes Adventure")
+
+class Background(pygame.sprite.Sprite):
+      def __init__(self):
+            super().__init__()
+
+
+class Ground(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+
+
+class Enemy(pygame.sprite.Sprite):
+      def __init__(self):
+        super().__init__()
+while True:
+
+    for event in pygame.event.get():
+        # Will run when the close window button is clicked
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+
+        # For events that occur upon clicking the mouse (left click)
+        if event.type == pygame.MOUSEBUTTONDOWN:
+              pass
+
+        # Event handling for a range of different key presses
+        if event.type == pygame.KEYDOWN:
+              pass
